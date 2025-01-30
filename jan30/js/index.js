44f3f84bcef23ee3095fbd5aee954c156a448831
KARVENDHAN <imkarvendhan@gmail.com>
2025-01-30 10:47:33 +0300

    Add files via upload

---
+++ jan30/js/index.js
@@ -0,0 +1,90 @@
+// console.log(true);
+
+//  Inbuild function
+//  alert("Hello")
+
+// let x = 10;
+
+// console.log(x);
+
+// let uname = "john"
+
+// console.log("Hi" + " " + uname);
+
+// let firstName = "vinoth" ,lastName="kumar";
+// console.log("firstName:" +firstName+""+"lastName:"+lastName)
+// console.log("firstName:"+firstName+""+lastName);
+
+// console.log('firstName:${firstName}+${lastName}')
+
+// console.log(`Firstname : ${firstName} Lastname : ${lastName}`);
+
+ //console.log(typeof 0.97876767);
+
+//console.log(typeof "hello");
+
+//console.log(typeof false);
+
+// let a = null;
+
+//  let b;
+
+// console.log(b); // undefined
+
+let a = 10;
+let b = 10;
+
+// console.log(a <= b);
+
+
+// console.log(a > b);
+// console.log(a<b);
+
+
+
+
+//console.log( a != b); // true
+
+//console.log(a == b); //check only data
+
+// console.log(a === b); // check data and datatype
+
+
+
+
+
+
+// post increment
+// console.log(a++);
+// console.log(a);
+
+// pre increment
+// console.log(++a);
+
+
+
+
+// console.log(a ** 3);  a3
+
+// a,b - operand
+// + - operator
+
+// +=
+
+let cart = 200;
+
+ cart = cart + 100
+cart += 100
+
+console.log(cart);
+
+
+
+
+
+
+
+
+
+
+
